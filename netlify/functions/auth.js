bc31c89faf39f35d71929348e3d82fd3c759fd52
ramannepal68 <ramannepal68@gmail.com>
2025-07-07 03:11:10 +1000

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,14 +1,15 @@
-exports.handler = async function (event, context) {
-  const clientId = process.env.CLIENT_ID;
-  const redirectUri = process.env.REDIRECT_URI;
-  const scope = encodeURIComponent('offline_access https://graph.microsoft.com/Mail.Read');
+const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const SCOPES = 'offline_access https://graph.microsoft.com/Mail.Read';
 
-  const authUrl = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&response_mode=query&scope=${scope}`;
+exports.handler = async (event) => {
+  const authUrl = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&scope=${encodeURIComponent(SCOPES)}&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_mode=query`;
 
   return {
     statusCode: 302,
     headers: {
-      Location: authUrl,
+      Location: authUrl
     },
+    body: ''
   };
 };
